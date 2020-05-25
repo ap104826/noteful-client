6756436e309b5bce14c5303aa87d2792e3677ad0
Arpita Mehta <arpita.patel02@gmail.com>
2020-05-24 17:23:35 -0700

    fix prod url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export default {
-  API_ENDPOINT: process.env.NODE_ENV === 'production' ? 'https://https://desolate-dawn-57125.herokuapp.com' : 'http://localhost:9090'
+  API_ENDPOINT: process.env.NODE_ENV === 'production' ?
+    'https://desolate-dawn-57125.herokuapp.com' : 'http://localhost:9090'
 }
