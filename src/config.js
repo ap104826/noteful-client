38fa478cc2dfcb18ef4b5e3362a9ca718abc00ae
Arpita Mehta <arpita.patel02@gmail.com>
2020-06-25 13:24:09 -0700

    changed config.sys file

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: process.env.NODE_ENV === 'production' ?
-    'https://desolate-dawn-57125.herokuapp.com/api' : 'http://localhost:9090/api'
+    'https://desolate-dawn-57125.herokuapp.com/api' : 'http://localhost:9090'
 }
